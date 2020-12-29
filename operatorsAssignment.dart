4efdbe0992721c5b230f5af69da4d23bd82a64e8
Jahidul-Bin-Rafiq <jahidulrafiq@gmail.com>
2020-12-29 11:32:47 +0600

    operators

---
+++ operatorsAssignment.dart
@@ -0,0 +1,5 @@
+main() {
+  var check = 60;
+  var compareCheck = (check >= 8) && (check < 75);
+  print(compareCheck);
+}
---
+++ tempatureCoversion.dart
@@ -0,0 +1,5 @@
+main() {
+  var fahrenheit = 56;
+  var celcius = (fahrenheit - 32) * (5 / 9);
+  print(celcius);
+}
